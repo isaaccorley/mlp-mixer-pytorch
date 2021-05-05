de146ab06f55e5b0e9a0a2325365086c2cab6e17
IsaacCorley <isaac.a.corley@gmail.com>
2021-05-04 23:23:24 -0500

    added model and configs

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # mlp-mixer-pytorch
-PyTorch implementation of "MLP-Mixer: An all-MLP Architecture for Vision" Tolstikhin et al. (2021)
+PyTorch implementation of ["MLP-Mixer: An all-MLP Architecture for Vision" Tolstikhin et al. (2021)](https://arxiv.org/abs/2105.01601)
+
+<img src="./assets/architecture.png" width="500px"></img>
+
+## Usage
+
+```python
+import torch
+from mlp_mixer import MLPMixer
+
+model = MLPMixer(
+    num_classes = 10,
+    num_layers = 8,
+    image_size = 256,
+    patch_size = 32,
+    channels = 3,
+    hidden_dim = 512,
+    tokens_hidden_dim = 256,
+    channels_hidden_dim = 2048
+)
+
+x = torch.randn(1, 3, 256, 256)
+model(x) # (1, 10)
+```
+
+# Install
+
+```bash
+pip install -r requirements.txt
+```
---
+++ mlp_mixer/mlp_mixer.py
@@ -0,0 +1,128 @@
+import einops
+import torch
+import torch.nn as nn
+from einops.layers.torch import Rearrange
+
+
+class PatchEmbeddings(nn.Module):
+
+    def __init__(
+        self,
+        patch_size: int,
+        hidden_dim: int,
+        channels: int
+    ):
+        super().__init__()
+        self.proj = nn.Sequential(
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=hidden_dim,
+                kernel_size=patch_size,
+                stride=patch_size
+            ),
+            Rearrange("b c h w -> b (h w) c")
+        )
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.proj(x)
+
+
+class Pooling(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x.mean(dim=1)
+
+
+class Classifier(nn.Module):
+
+    def __init__(self, input_dim: int, num_classes: int):
+        super().__init__()
+        self.model = nn.Linear(input_dim, num_classes)
+        nn.init.zeros_(self.model.weight)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
+
+
+class MLPBlock(nn.Module):
+
+    def __init__(self, input_dim: int, hidden_dim: int):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.GELU(),
+            nn.Linear(hidden_dim, input_dim)
+        )
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)
+
+
+class MixerBlock(nn.Module):
+
+    def __init__(
+        self,
+        num_patches: int,
+        num_channels: int,
+        tokens_hidden_dim: int,
+        channels_hidden_dim: int
+    ):
+        super().__init__()
+        self.token_mixing = nn.Sequential(
+            nn.LayerNorm(num_channels),
+            Rearrange("b p c -> b c p"),
+            MLPBlock(num_patches, tokens_hidden_dim),
+            Rearrange("b c p -> b p c")
+        )
+        self.channel_mixing = nn.Sequential(
+            nn.LayerNorm(num_channels),
+            MLPBlock(num_channels, channels_hidden_dim)
+        )
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x + self.token_mixing(x)
+        x = x + self.channel_mixing(x)
+        return x
+
+
+class MLPMixer(nn.Module):
+
+    def __init__(
+        self,
+        num_classes: int,
+        num_layers: int = 8,
+        image_size: int = 256,
+        patch_size: int = 32,
+        channels: int = 3,
+        hidden_dim: int = 512,
+        tokens_hidden_dim: int = 256,
+        channels_hidden_dim: int = 2048
+    ):
+        super().__init__()
+        num_patches = (image_size // patch_size) ** 2
+        self.embed = PatchEmbeddings(patch_size, hidden_dim, channels)
+        layers = [
+            MixerBlock(
+                num_patches=num_patches,
+                num_channels=hidden_dim,
+                tokens_hidden_dim=tokens_hidden_dim,
+                channels_hidden_dim=channels_hidden_dim
+            )
+            for _ in range(num_layers)
+        ]
+        self.layers = nn.Sequential(*layers)
+        self.norm = nn.LayerNorm(hidden_dim)
+        self.pool = Pooling()
+        self.classifier = Classifier(hidden_dim, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        b, c, h, w = x.shape
+        x = self.embed(x)           # [b, p, c]
+        x = self.layers(x)          # [b, p, c]
+        x = self.norm(x)            # [b, p, c]
+        x = self.pool(x)            # [b, c]
+        x = self.classifier(x)      # [b, num_classes]
+        return x
