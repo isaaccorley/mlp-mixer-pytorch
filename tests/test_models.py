a09b68f8909bfdca81553642e7a8ee6c520b88dd
IsaacCorley <isaac.a.corley@gmail.com>
2021-05-12 20:34:18 -0500

    updated tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -12,49 +12,49 @@ x = x.to(DEVICE)
 
 
 def test_mlp_mixer_s16():
-    model = mlp_mixer.mlp_mixer_s16(**PARAMS)
+    model = mlp_mixer.models.mlp_mixer_s16(**PARAMS)
     model = model.to(DEVICE)
     y_hat = model(x)
     assert y_hat.shape == (1, PARAMS["num_classes"])
     assert y_hat.dtype == torch.float32
 
 def test_mlp_mixer_s32():
-    model = mlp_mixer.mlp_mixer_s32(**PARAMS)
+    model = mlp_mixer.models.mlp_mixer_s32(**PARAMS)
     model = model.to(DEVICE)
     y_hat = model(x)
     assert y_hat.shape == (1, PARAMS["num_classes"])
     assert y_hat.dtype == torch.float32
 
 def test_mlp_mixer_b16():
-    model = mlp_mixer.mlp_mixer_b16(**PARAMS)
+    model = mlp_mixer.models.mlp_mixer_b16(**PARAMS)
     model = model.to(DEVICE)
     y_hat = model(x)
     assert y_hat.shape == (1, PARAMS["num_classes"])
     assert y_hat.dtype == torch.float32
 
 def test_mlp_mixer_b32():
-    model = mlp_mixer.mlp_mixer_b32(**PARAMS)
+    model = mlp_mixer.models.mlp_mixer_b32(**PARAMS)
     model = model.to(DEVICE)
     y_hat = model(x)
     assert y_hat.shape == (1, PARAMS["num_classes"])
     assert y_hat.dtype == torch.float32
 
 def test_mlp_mixer_l16():
-    model = mlp_mixer.mlp_mixer_l16(**PARAMS)
+    model = mlp_mixer.models.mlp_mixer_l16(**PARAMS)
     model = model.to(DEVICE)
     y_hat = model(x)
     assert y_hat.shape == (1, PARAMS["num_classes"])
     assert y_hat.dtype == torch.float32
 
 def test_mlp_mixer_l32():
-    model = mlp_mixer.mlp_mixer_l32(**PARAMS)
+    model = mlp_mixer.models.mlp_mixer_l32(**PARAMS)
     model = model.to(DEVICE)
     y_hat = model(x)
     assert y_hat.shape == (1, PARAMS["num_classes"])
     assert y_hat.dtype == torch.float32
 
 def test_mlp_mixer_h14():
-    model = mlp_mixer.mlp_mixer_h14(**PARAMS)
+    model = mlp_mixer.models.mlp_mixer_h14(**PARAMS)
     model = model.to(DEVICE)
     y_hat = model(x)
     assert y_hat.shape == (1, PARAMS["num_classes"])
